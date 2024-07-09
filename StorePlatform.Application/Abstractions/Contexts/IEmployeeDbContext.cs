ea0455e9d5011a89e81dab9be3cd4ae4560ad064
Muhammed Nur Alsammour <muhammed2005nour@gmail.com>
2024-07-09 19:07:29 +0300

    Result

--- StorePlatform.Application/Abstractions/Contexts/IEmployeeDbContext.cs
+++ StorePlatform.Application/Abstractions/Contexts/IEmployeeDbContext.cs
@@ -10,6 +10,9 @@ namespace StorePlatform.Application.Abstractions.Contexts
 		public DbSet<Employee> Employees { get; set; }
 		public DbSet<Example> Examples { get; set; }
 		public DbSet<Customer> Customers { get; set; }
+		public DbSet<Product> Products { get; set; }
+		public DbSet<Category> categories { get; set; }
+		public DbSet<ProductCategory> ProductCategories { get; set; }
 
 		Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
 	}
--- StorePlatform.Persistence/Contexts/EmployeeDbContext.cs
+++ StorePlatform.Persistence/Contexts/EmployeeDbContext.cs
@@ -11,12 +11,34 @@ namespace StorePlatform.Persistence.Contexts
 		public DbSet<Employee> Employees { get; set; }
 		public DbSet<Example> Examples { get; set; }
 		public DbSet<Customer> Customers { get; set; }
+		public DbSet<Product> Products { get; set; }
+		public DbSet<Category> categories { get; set; }
+		public DbSet<ProductCategory> ProductCategories { get; set; }
 
 
 		protected override void OnModelCreating(ModelBuilder modelBuilder)
 		{
 			modelBuilder.HasDefaultSchema("StorePlatform");
 			base.OnModelCreating(modelBuilder);
+
+			modelBuilder.Entity<ProductCategory>()
+				.ToTable("product_categories");
+
+			modelBuilder.Entity<ProductCategory>()
+				.HasKey(pc => new { pc.ProductId, pc.CategoryId });
+
+			modelBuilder.Entity<ProductCategory>()
+				.HasOne(pc => pc.Product)
+				.WithMany(p => p.ProductCategories)
+				.HasForeignKey(pc => pc.ProductId);
+
+			modelBuilder.Entity<ProductCategory>()
+				.HasOne(pc => pc.Category)
+				.WithMany(c => c.ProductCategories)
+				.HasForeignKey(pc => pc.CategoryId);
 		}
+
+
+
 	}
 }
